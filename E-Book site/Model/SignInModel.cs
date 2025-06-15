7a8ede830521411a180b26bd2487db13f0172805
Umair Khalid <itsumairkhalid@gmail.com>
2025-06-15 22:04:01 +0500

    authentication completed

---
+++ E-Book site/Model/SignInModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_Book_site.Model
+{
+    public class SignInModel
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; } = string.Empty;
+    }
+}
