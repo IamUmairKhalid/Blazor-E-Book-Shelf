141eea73d3fc3f9a67e7bc63e2d3a34bd698a45e
Umair Khalid <itsumairkhalid@gmail.com>
2025-06-17 00:06:44 +0500

    updated Admin Dashboar and Logs table

--- E-Book site/Model/BooksLogs.cs
+++ E-Book site/Model/BooksLogs.cs
@@ -18,7 +18,7 @@ namespace E_Book_site.Model
         public DateTime? UpdatedAt { get; set; }
 
         [Range(0, 5, ErrorMessage = "Rating must be between 0 and 5.")]
-        public double? Rating { get; set; }
+        public double? Rating { get; set; } = 0.0;
 
         public bool Favorite { get; set; } = false;
 
@@ -27,10 +27,10 @@ namespace E_Book_site.Model
         public string Status { get; set; } = "NotOpend";
 
         [Range(1, int.MaxValue, ErrorMessage = "Last seen page must be 1 or greater.")]
-        public int? LastSeenPage { get; set; }
+        public int? LastSeenPage { get; set; } = 1;
 
         [Range(0, 100, ErrorMessage = "Progress must be between 0 and 100 percent.")]
-        public double? ProgressPercentage { get; set; }
+        public double? ProgressPercentage { get; set; } = 0.0;
 
         public Users User { get; set; }
         public Book Book { get; set; }
