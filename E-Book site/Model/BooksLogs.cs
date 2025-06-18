a718cdeceed285004bf8c1a933ef546f479dea99
Umair Khalid <itsumairkhalid@gmail.com>
2025-06-18 11:29:33 +0500

    Removed Bugs and Refined Code

--- E-Book site/Model/BooksLogs.cs
+++ E-Book site/Model/BooksLogs.cs
@@ -27,7 +27,7 @@ namespace E_Book_site.Model
         public string Status { get; set; } = "NotOpend";
 
         [Range(1, int.MaxValue, ErrorMessage = "Last seen page must be 1 or greater.")]
-        public int? LastSeenPage { get; set; } = 1;
+        public int? LastSeenPage { get; set; } = 0;
 
         [Range(0, 100, ErrorMessage = "Progress must be between 0 and 100 percent.")]
         public double? ProgressPercentage { get; set; } = 0.0;
